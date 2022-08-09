dd99b9f9c17804bdb26be7cb8e63b1982b030bb2
mikefront <mike_front@mail.ru>
2022-08-09 19:06:30 +0500

    начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задание1
-        int number = 1;
-        while (number <= 10) {
-            System.out.print(number++);
-        }
-        {
-            System.out.println();
-        }
-        for (int someNumber = 10; someNumber >= 1; someNumber-- ) {
-            System.out.print(+ someNumber);
-        }
     }
 }
